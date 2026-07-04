6013a3cef7c382a84c0ca622c5d751a5b94153c1
Aris <aris.niamonitakis@gmail.com>
2026-07-04 14:17:50 +0100

    prototype practice exercise

---
+++ C/C_practice_files/prototype_practice.c
@@ -0,0 +1,28 @@
+
+// 5. Function Prototype Practice
+
+// Put your function definitions below main().
+
+// Add prototypes above main().
+
+// Example:
+
+#include <stdio.h>
+
+int sum(int a, int b);
+
+
+int main(){
+    int z = 5;
+    int y = 6;
+    int x = sum(z,y);
+    printf("the sum of %d and %d is %d",z,y,x);
+
+
+
+
+}
+
+int sum(int a, int b){
+    return a+b;
+}
